8f4c3e826390694889df7776581ffc45421e52e2
Nathan Bishop <nbish11@hotmail.com>
2020-12-13 18:14:32 +1000

    Create a mailbox-list

---
+++ src/MailboxList.php
@@ -0,0 +1,105 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Email;
+
+use Meraki\Email\Mailbox;
+use Countable;
+use IteratorAggregate;
+use InvalidArgumentException;
+use Iterator;
+use ArrayIterator;
+use RangeException;
+
+// A "group-list" is also a "mailbox-list"
+final class MailboxList implements Countable, IteratorAggregate
+{
+	private $mailboxes;
+
+	public function __construct(Mailbox $mailbox, Mailbox ...$mailboxes)
+	{
+		$this->mailboxes = array_merge([$mailbox], $mailboxes);
+	}
+
+	public function add(Mailbox $mailbox): void
+	{
+		if ($this->contains($mailbox)) {
+			throw new InvalidArgumentException('Cannot add the same mailbox more than once.');
+		}
+
+		$this->mailboxes[] = $mailbox;
+	}
+
+	public function remove(Mailbox $mailbox): void
+	{
+		if (count($this->mailboxes) === 1) {
+			throw new RangeException('A mailbox-list must have at least one mailbox.');
+		}
+
+		$index = $this->indexOf($mailbox);
+
+		if ($index >= 0) {
+			unset($this->mailboxes[$index]);
+
+			return;
+		}
+
+		throw new InvalidArgumentException('Cannot remove an mailbox that does not exist in the list.');
+	}
+
+	public function contains(Mailbox $mailbox): bool
+	{
+		foreach ($this->mailboxes as $storedMailbox) {
+			if ($storedMailbox->equals($mailbox)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function indexOf(Mailbox $mailbox): int
+	{
+		foreach ($this->mailboxes as $index => $storedMailbox) {
+			if ($storedMailbox->equals($mailbox)) {
+				return $index;
+			}
+		}
+
+		return -1;
+	}
+
+	public function equals(self $other): bool
+	{
+		if (count($this->mailboxes) === count($other->mailboxes)) {
+            foreach ($this->mailboxes as $thisMailbox) {
+                foreach ($other->mailboxes as $otherMailbox) {
+                    if ($thisMailbox->equals($otherMailbox)) {
+                        continue 2;
+                    }
+                }
+                
+                return false;
+            }
+            
+            return true;
+        }
+        
+        return false;
+	}
+
+	public function count(): int
+	{
+		return count($this->mailboxes);
+	}
+
+	public function getIterator(): Iterator
+	{
+		return new ArrayIterator($this->mailboxes);
+	}
+
+	public function __toString(): string
+	{
+		return implode(',', $this->mailboxes);
+	}
+}
---
+++ tests/MailboxListTest.php
@@ -0,0 +1,201 @@
+<?php
+declare(strict_types=1);
+
+namespace Meraki\Email;
+
+use Meraki\Email\MailboxList;
+use Meraki\Email\Mailbox;
+use Meraki\TestSuite\TestCase;
+use Countable;
+use IteratorAggregate;
+use RangeException;
+
+final class MailboxListTest extends TestCase
+{
+	/**
+	 * @test
+	 */
+	public function mailbox_provided_during_creation_is_added(): void
+	{
+		$mailbox = Mailbox::withoutDisplayName('test@example.com');
+
+		$mailboxList = new MailboxList($mailbox);
+
+		$this->assertNotEmpty($mailboxList);
+		$this->assertCount(1, $mailboxList);
+		$this->assertTrue($mailboxList->contains($mailbox));
+	}
+
+	/**
+	 * @test
+	 */
+	public function can_be_counted(): void
+	{
+		$mailbox = Mailbox::withoutDisplayName('test@example.com');
+
+		$mailboxList = new MailboxList($mailbox);
+
+		$this->assertInstanceOf(Countable::class, $mailboxList);
+	}
+
+	/**
+	 * @test
+	 */
+	public function can_be_looped_over(): void
+	{
+		$mailbox = Mailbox::withoutDisplayName('test@example.com');
+
+		$mailboxList = new MailboxList($mailbox);
+
+		$this->assertInstanceOf(IteratorAggregate::class, $mailboxList);
+	}
+
+	/**
+	 * @test
+	 */
+	public function adding_a_mailbox_increases_the_count(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList = new MailboxList($mailbox1);
+
+		$mailboxList->add($mailbox2);
+
+		$this->assertCount(2, $mailboxList);
+	}
+
+	/**
+	 * @test
+	 */
+	public function mailbox_exists_when_added(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList = new MailboxList($mailbox1);
+
+		$mailboxList->add($mailbox2);
+
+		$this->assertTrue($mailboxList->contains($mailbox2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function removing_a_mailbox_decreases_the_count(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList = new MailboxList($mailbox1, $mailbox2);
+
+		$mailboxList->remove($mailbox2);
+
+		$this->assertCount(1, $mailboxList);
+	}
+
+	/**
+	 * @test
+	 */
+	public function mailbox_doesnt_exist_when_removed(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList = new MailboxList($mailbox1, $mailbox2);
+
+		$mailboxList->remove($mailbox2);
+
+		$this->assertFalse($mailboxList->contains($mailbox2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function list_cannot_be_made_empty_by_removing_mailboxes(): void
+	{
+		$exception = new RangeException('A mailbox-list must have at least one mailbox.');
+		$mailbox = Mailbox::withoutDisplayName('test@example.com');
+		$mailboxList = new MailboxList($mailbox);
+
+		$this->expectExceptionObject($exception);
+
+		$mailboxList->remove($mailbox);
+	}
+
+	/**
+	 * @test
+	 */
+	public function mailbox_doesnt_exist_in_list_if_not_added(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList = new MailboxList($mailbox1);
+
+		$this->assertFalse($mailboxList->contains($mailbox2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function two_mailbox_lists_are_not_equal_if_they_differ_in_mailbox_count(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList1 = new MailboxList($mailbox1, $mailbox2);
+		$mailboxList2 = new MailboxList($mailbox1);
+
+		$this->assertFalse($mailboxList1->equals($mailboxList2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function two_mailbox_lists_are_not_equal_if_they_have_different_mailboxes(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList1 = new MailboxList($mailbox1);
+		$mailboxList2 = new MailboxList($mailbox2);
+
+		$this->assertFalse($mailboxList1->equals($mailboxList2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function two_mailbox_lists_are_equal_if_they_have_same_mailboxes(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList1 = new MailboxList($mailbox1, $mailbox2);
+		$mailboxList2 = new MailboxList($mailbox1, $mailbox2);
+
+		$this->assertTrue($mailboxList1->equals($mailboxList2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function two_mailbox_lists_are_equal_if_they_have_same_mailboxes_regardless_of_order(): void
+	{
+		$mailbox1 = Mailbox::withoutDisplayName('test1@example.com');
+		$mailbox2 = Mailbox::withoutDisplayName('test2@example.com');
+		$mailboxList1 = new MailboxList($mailbox1, $mailbox2);
+		$mailboxList2 = new MailboxList($mailbox2, $mailbox1);
+
+		$this->assertTrue($mailboxList1->equals($mailboxList2));
+	}
+
+	/**
+	 * @test
+	 */
+	public function casting_to_string_returns_mailboxes_separated_by_commas(): void
+	{
+		$mailboxList = new MailboxList(
+			Mailbox::withDisplayName('Nathan Bishop', 'nbish11@hotmail.com'),
+			Mailbox::withoutDisplayName('test@example.com')
+		);
+
+		$builtMailboxList = (string) $mailboxList;
+
+		$this->assertEquals('"Nathan Bishop"<nbish11@hotmail.com>,<test@example.com>', $builtMailboxList);
+	}
+}
